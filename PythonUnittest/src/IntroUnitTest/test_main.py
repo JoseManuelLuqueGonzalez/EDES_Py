d6ab46b858080a74008e03b96ac619c73d9af8c8
Lainezz <diegolinaresmojacar@gmail.com>
2022-11-30 16:50:59 +0100

    codigo clase 30 del 11__correccion fallo lol

--- PythonUnittest/src/IntroUnitTest/test_main.py
+++ PythonUnittest/src/IntroUnitTest/test_main.py
@@ -1,13 +1,13 @@
-import suma as s
+import aritmetica as arit
 import unittest
 
 class TestSuma(unittest.TestCase):
 
     """La conveción para declarar tests, es empezar por la palabra test"""
     def test_suma(self):
-        self.assertEqual(s.suma(2,2), 4)
-        self.assertEqual(s.suma(-1,1), 0)
-        self.assertEqual(s.suma(-1,-1), -2)
+        self.assertEqual(arit.suma(2,2), 4)
+        self.assertEqual(arit.suma(-1,1), 0)
+        self.assertEqual(arit.suma(-1,-1), -2)
 
 
 if __name__ == "__main__":
